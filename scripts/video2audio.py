c5377fe1848817b4003a0677ddbd9a6a5214afd9
Aisu Wata <aisu.wata0@gmail.com>
2023-07-28 22:05:09 -0300

    video2audio generic file support

--- scripts/video2audio.py
+++ scripts/video2audio.py
@@ -11,14 +11,18 @@ filelist = list(os.walk(video_dir))[0][2]
 def generate_infos():
     videos = []
     for file in filelist:
-        if file.endswith(".mp4"):
-            videos.append(file)
+        videos.append(file)
     return videos
 
 
 def clip_file(file):
-    my_audio_clip = AudioFileClip(video_dir + file)
-    my_audio_clip.write_audiofile(audio_dir + file.rstrip(".mp4") + ".wav")
+    filepath =  video_dir + file
+    try:
+        my_audio_clip = AudioFileClip(filepath)
+        my_audio_clip.write_audiofile(audio_dir + file.rstrip(".mp4") + ".wav")
+    except Exception as e:
+        print(e)
+        print(f'Failed to get audio from file "{filepath}"')
 
 
 if __name__ == "__main__":
