7fc83324d68e0fa6466115ffb46f6355e1784cf5
Aisu Wata <aisu.wata0@gmail.com>
2023-07-28 22:09:55 -0300

    download video with yt-dlp instead

--- scripts/download_video.py
+++ scripts/download_video.py
@@ -3,32 +3,43 @@ import random
 import shutil
 from concurrent.futures import ThreadPoolExecutor
 from google.colab import files
+import re
 
 basepath = os.getcwd()
 uploaded = files.upload()  # 上传文件
 for filename in uploaded.keys():
     assert (filename.endswith(".txt")), "speaker-videolink info could only be .txt file!"
-    shutil.move(os.path.join(basepath, filename), os.path.join("./speaker_links.txt"))
+    filpath_dest = os.path.join("./speaker_links.txt")
+    print("Moving file to ", filpath_dest)
+    shutil.move(os.path.join(basepath, filename), filpath_dest)
 
 
 def generate_infos():
     infos = []
     with open("./speaker_links.txt", 'r', encoding='utf-8') as f:
         lines = f.readlines()
+    print("lines", lines)
     for line in lines:
         line = line.replace("\n", "").replace(" ", "")
         if line == "":
             continue
         speaker, link = line.split("|")
+        # link_sanitized = '-'.join(re.findall(r'\w+', link))
         filename = speaker + "_" + str(random.randint(0, 1000000))
-        infos.append({"link": link, "filename": filename})
+        info = {"link": link, "filename": filename}
+        print("speaker, link")
+        print(speaker, link, info)
+        infos.append(info)
     return infos
 
 
 def download_video(info):
     link = info["link"]
     filename = info["filename"]
-    os.system(f"youtube-dl -f 0 {link} -o ./video_data/{filename}.mp4 --no-check-certificate")
+    command = f'yt-dlp {link} -o "./video_data/{filename}.%(ext)s" -f "(bestaudio)" -x  --no-check-certificate'
+    print("download_video", info)
+    print("command", command)
+    os.system(command)
 
 
 if __name__ == "__main__":
